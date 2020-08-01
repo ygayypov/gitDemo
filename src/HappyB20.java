f29e524a59ee5f556cf8ecaba2373ef6e9b5a0bb
ygayypov <yarid_gara@hotmail.com>
2020-08-01 09:31:01 -0600

    @BI_23 add Login.java

---
+++ src/HappyB20.java
@@ -0,0 +1,8 @@
+
+
+public class HappyB20 {
+    public static void main(String[] args) {
+        System.out.println("Best Regards!!!");
+    }
+
+}
---
+++ src/HappyBB20.java
@@ -0,0 +1,6 @@
+public class HappyBB20 {
+    public static void main(String[] args) {
+        System.out.println("B20 is always happy");
+
+    }
+}
