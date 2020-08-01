463e3c5b82049c4b32428e7ee5eb2ecb19e5c716
ygayypov <yarid_gara@hotmail.com>
2020-08-01 13:36:26 -0600

    add BookARoom

---
+++ src/BookARoom.java
@@ -0,0 +1,11 @@
+public class BookARoom {
+    public static void main(String[] args) {
+        int day = 0;
+        for (int i = 0; i <= 10; i++){
+            if (i % 2 != 0){
+                day++;
+            }
+        }
+        System.out.println(day);
+    }
+}
